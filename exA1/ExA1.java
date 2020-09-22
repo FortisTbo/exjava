05edddeb6511d4baf395c438e34afce9e17912ea
FortisTbo <mail4tbo@gmail.com>
2020-09-22 09:49:45 +0200

    exA1

---
+++ exA1/ExA1.java
@@ -0,0 +1,16 @@
+public class ExA1 {
+	
+	 public static void main (String [] args) {
+		 System.out.println("Hello");
+		 
+		 String title = new String ("Java");
+		 int numberOfDays = 3;
+		 double pricePerDay = 120.00;
+		 boolean priorKnowledgeRequired = false;
+		 
+		 System.out.println ("Title=" + title + 
+								" / Number of days=" + numberOfDays + 
+								" / Price per day=" + pricePerDay +
+								" / Prior knowledge is required=" + priorKnowledgeRequired);
+	 }
+}
